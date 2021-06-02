aad87770c81879e5212fbd390de774ab9d85c46b
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-02 19:16:56 +0000

    moved client fn to client.js and export to play.js

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
-
-//connection with game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected!");
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
